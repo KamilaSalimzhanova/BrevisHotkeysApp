8a81edaa0ae7a77dbce7fe6c4dd8081c5af3db39
kamila <ksalimzh@gmail.com>
2025-03-28 00:27:30 +0500

    Initial Commit

---
+++ BrevisHotkeysApp/BrevisHotkeysAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrevisHotkeysAppApp.swift
+//  BrevisHotkeysApp
+//
+//  Created by kamila on 28.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct BrevisHotkeysAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
